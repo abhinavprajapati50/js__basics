5d23f2a100fce3b2194bdb6c214acd4e96371b07
Chirag B <chirag.gallitein@ken42.com>
2022-04-20 19:23:49 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { SliderComponent } from './component/Slider';
+
+function App() {
+  return (
+    // <div className="App">
+      <div >
+      <SliderComponent />
+      <JavscriptBasics />
+      </div>
+    // </div>
+  );
+}
+
+export default App;
---
+++ src/component/JavscriptBasics.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const JavscriptBasics = () => {
+  return (
+      <div>
+          ------------------------------------JavscriptBasics------------------------------------
+          --------1) Pig Game
+      </div>
+  )
+}
+
